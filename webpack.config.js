c06c1c741dad9dd0200b8ae306a90c0ca3fd276b
DESKTOP-UD6SLEN\redcamel <webseon@gmail.com>
2019-10-10 01:14:11 +0900

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
 const path = require('path');
 const webpack = require("webpack");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-module.exports = {
-	mode: 'none',
-	entry: {
-		"RedTimer": './src/RedTimer.js',
-		"RedTimer.min": './src/RedTimer.js'
-	},
-	output: {
-		library: 'RedTimer',
-		libraryTarget: "umd",
-		libraryExport: "default",
-		umdNamedDefine: true,
-		globalObject: "typeof self !== 'undefined' ? self : this",
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist')
-	},
-	optimization: {
-		namedModules: true,
-		mangleWasmImports: true,
-		minimize: true,
-		minimizer: [new UglifyJsPlugin({
-			include: /\.min\.js$/,
-			uglifyOptions: {
-				compress: {
-					drop_console: true
+const fileName = 'RedTimer';
+module.exports = (function () {
+	var config = {
+		mode:'none',
+		entry : {},
+		output: {
+			library: 'RedTimer',
+			libraryTarget: "umd",
+			libraryExport: "default",
+			umdNamedDefine: true,
+			globalObject: "typeof self !== 'undefined' ? self : this",
+			filename: '[name].js',
+			path: path.resolve(__dirname, 'dist')
+		},
+		optimization: {
+			namedModules: true,
+			mangleWasmImports: true,
+			minimize: true,
+			minimizer: [new UglifyJsPlugin({
+				include: /\.min\.js$/,
+				uglifyOptions: {
+					compress: {
+						drop_console: true
+					}
 				}
-			}
-		})]
-	}
-};
+			})]
+		}
+	};
+	config.entry[fileName] = `./src/${fileName}.js`;
+	config.entry[fileName+'.min'] = `./src/${fileName}.js`;
+	return config
+})();
